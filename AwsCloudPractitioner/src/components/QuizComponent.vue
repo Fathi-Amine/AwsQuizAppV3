139904fa0898046808598bd483322362e8cc4dfe
Fathi-Amine <aminefathimw@gmail.com>
2023-03-06 23:25:21 +0100

    updated

--- AwsCloudPractitioner/src/components/QuizComponent.vue
+++ AwsCloudPractitioner/src/components/QuizComponent.vue
@@ -31,8 +31,15 @@
         }),
         methods:{
             displayQuestions(){
-                
-            }
+                let questions = data.results.map((question) => {
+                    for (let i = question.answers.length - 1; i > 0; i--) {
+                        const j = Math.floor(Math.random() * (i + 1));
+                        [question.answers[i], question.answers[j]] = [
+                            question.answers[j],
+                            question.answers[i],
+                        ];
+                    }
+            })
         }
     }
 </script>
